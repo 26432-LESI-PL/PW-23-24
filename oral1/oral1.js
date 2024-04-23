bc03e70c914efd95aefeb166d2aa8d71bcc947a8
danielalexis <danielalexis.o.pereira@gmail.com>
2024-04-23 20:51:39 +0100

    Refactor button onclick event in index.html to use button id

--- oral1/oral1.js
+++ oral1/oral1.js
@@ -1,8 +1,12 @@
 const words = ["IPCA", "João", "Joana", "Rui", "Pereira", "Uma árvore", "Programação Web"]
 const text = document.getElementById("word")
+const button = document.getElementById("button")
+
 
 function showWord() {
   const index = Math.floor(Math.random() * words.length)
   text.style.display = "block"
   document.getElementById("word").innerText = words[index]
 }
+
+button.addEventListener("click", showWord)
